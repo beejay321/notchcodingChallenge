24b327ccc248bd3ad4a3de972aeaed74a39a3702
beejay321 <jibodu.busola@gmail.com>
2022-10-21 17:32:30 +0200

    Update App.js
    Changing date format. and adding classes to elements

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Input, Button } from "reactstrap";
+import "./style.css";
 // const BACKEND_URL = process.env.REACT_APP_BACKENDURL;
 const BACKEND_URL = "http://localhost:3001";
 
@@ -10,6 +11,7 @@ const INIT_VALUE = {
 function App() {
   const [text, setText] = useState(INIT_VALUE);
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState([]);
 
   const handleChange = (evt) => {
     setText({ ...text, [evt.target.name]: evt.target.value });
@@ -25,26 +27,21 @@ function App() {
     console.log(response);
   };
 
-  // const handleDateConvert = (date) => {
-  //   date.split("z");
-  //      let convertDate = new Date(date);
-  //   convertDate.getDate()
-  //   console.log(convertDate)
-  //   return convertDate;
-  // };
+  const handleDateConvert = (date) => {
+    const convert = new Date(date);
+    const dateConverted = convert.toISOString().substring(0, 10);
+    return dateConverted.replaceAll("-", ".");
+  };
 
   useEffect(() => {
-    // setIsLoading(true);
-
+    setIsLoading(true);
     const getPosts = async () => {
       try {
         let response = await fetch(`${BACKEND_URL}/posts`, {});
         if (response.ok) {
           let data = await response.json();
-          console.log(data.posts);
-
           setPosts(data.posts);
-          // setIsLoading(false);
+          setIsLoading(false);
         }
       } catch (error) {
         console.log(error);
@@ -56,23 +53,30 @@ function App() {
   return (
     <>
       <Container>
-        <h1>Create new Article</h1>
-        <Input className="mb-2" name="title" value={text.title} onChange={handleChange} />
-        <Input id="exampleText" type="textarea" className="mb-2" name="description" value={text.description} onChange={handleChange} />
-        <Button color="primary" onClick={handleSubmit}>
-          Create
-        </Button>
+        <h3>Create new Article</h3>
+        <Input className="mb-2 articleInput" name="title" value={text.title} onChange={handleChange} />
+        <Input id="exampleText" type="textarea" className="mb-2 articleInput" name="description" value={text.description} onChange={handleChange} />
+
+        <div className="createBtnDiv">
+          <Button className="createBtn" color="primary" onClick={handleSubmit}>
+            Create
+          </Button>
+        </div>
       </Container>
 
       <Container>
-        <h3> Articles</h3>
+        <h3 className="sectionTitle"> Articles</h3>
         {posts &&
           posts.map((post) => (
-            <div>
-              <h3> {post.title}</h3>
-              <p> {post.description}</p>
-              {/* <h3> {post.createdAt}</h3> */}
-            </div>
+            <>
+              <div className="eachArticle">
+                <div className="articleHeading" key={post._id}>
+                  <p className="postTitle"> {post.title}</p>
+                  <p className="date"> {handleDateConvert(post.createdAt)}</p>
+                </div>
+                <p className="article"> {post.description}</p>
+              </div>
+            </>
           ))}
       </Container>
     </>
